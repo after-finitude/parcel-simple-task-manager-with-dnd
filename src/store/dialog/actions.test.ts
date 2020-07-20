42384c862d50b655a6f8967bd480c4c335743a7f
Alexander Zenov <alexander.zenov@gmail.com>
2020-07-20 22:42:25 +0300

    Add tests for dialog actions

---
+++ src/store/dialog/actions.test.ts
@@ -0,0 +1,64 @@
+import { openDialog, closeDialog } from "store/dialog/actions";
+import {
+  DialogTypes,
+  DialogActionTypes,
+  ActionOpenDialog,
+  ActionCloseDialog,
+} from "store/dialog/types";
+describe("Dialog actions", () => {
+  test("openDialog with type create", () => {
+    const input: ActionOpenDialog = openDialog(DialogTypes.CREATE, "1234234");
+    const output: ActionOpenDialog = {
+      payload: {
+        currentTaskId: "1234234",
+        status: true,
+        type: "create",
+      },
+      type: DialogActionTypes.OPEN_DIALOG,
+    };
+
+    expect(input).toEqual(output);
+  });
+
+  test("closeDialog with type create", () => {
+    const input: ActionCloseDialog = closeDialog(DialogTypes.CREATE);
+    const output: ActionCloseDialog = {
+      payload: {
+        currentTaskId: "",
+        status: false,
+        type: "create",
+      },
+      type: DialogActionTypes.CLOSE_DIALOG,
+    };
+
+    expect(input).toEqual(output);
+  });
+
+  test("openDialog with type update", () => {
+    const input: ActionOpenDialog = openDialog(DialogTypes.UPDATE, "32425");
+    const output: ActionOpenDialog = {
+      payload: {
+        currentTaskId: "32425",
+        status: true,
+        type: "update",
+      },
+      type: DialogActionTypes.OPEN_DIALOG,
+    };
+
+    expect(input).toEqual(output);
+  });
+
+  test("closeDialog with type update", () => {
+    const input: ActionCloseDialog = closeDialog(DialogTypes.UPDATE);
+    const output: ActionCloseDialog = {
+      payload: {
+        currentTaskId: "",
+        status: false,
+        type: "update",
+      },
+      type: DialogActionTypes.CLOSE_DIALOG,
+    };
+
+    expect(input).toEqual(output);
+  });
+});
