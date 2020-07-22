358714ab504ade5738ff8129db89a253c3719cce
Alexander Zenov <alexander.zenov@gmail.com>
2020-07-22 21:37:31 +0300

    Add tests for localStorage and update package.json

---
+++ src/store/localStorage.test.ts
@@ -0,0 +1,58 @@
+import { loadState, saveState } from "store/localStorage";
+
+class LocalStorageMock {
+  store: Record<string, string>;
+
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: string) {
+    this.store[key] = value;
+  }
+
+  removeItem(key: string) {
+    delete this.store[key];
+  }
+}
+
+describe("Local storage", () => {
+  beforeAll(() => {
+    localStorage = new LocalStorageMock() as any;
+
+    localStorage.setItem("state", "1");
+  });
+  test("loadState", () => {
+    const input = loadState();
+    const output = 1;
+
+    expect(input).toBe(output);
+  });
+
+  test("saveState", () => {
+    saveState({
+      tasks: [
+        { id: "1", title: "Test1", description: "test1", closed: false },
+        { id: "2", title: "Test2", description: "test2", closed: false },
+      ],
+    });
+
+    const input = localStorage.getItem("state");
+    const output = JSON.stringify({
+      tasks: [
+        { id: "1", title: "Test1", description: "test1", closed: false },
+        { id: "2", title: "Test2", description: "test2", closed: false },
+      ],
+    });
+
+    expect(input).toBe(output);
+  });
+});
