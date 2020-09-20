72a069ce4e14e977e1e0213af69db158d9e53c88
Alexander Zenov <alexander.zenov@gmail.com>
2020-09-20 18:13:14 +0300

    Update some tests

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render } from "enzyme";
 
 import { Header } from "components/Header";
 
 describe("Header", () => {
   it("should render correctly", () => {
-    const component = shallow(<Header />);
+    const component = render(<Header />);
 
     expect(component).toMatchSnapshot();
   });
--- src/components/Main/index.test.tsx
+++ src/components/Main/index.test.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 
 import { Main } from "components/Main";
 import { mockStore } from "utils/mocks/mockStore";
 
 describe("Main", () => {
   it("should render correctly", () => {
-    const component = mount(
+    const component = shallow(
       <Provider store={mockStore}>
         <Main />
       </Provider>,
