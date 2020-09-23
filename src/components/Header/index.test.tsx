210d4b7262a7a3031f798fa29927e17b1ffbab9a
Alexander Zenov <alexander.zenov@gmail.com>
2020-09-23 22:31:27 +0300

    Update old test and new test in Header component

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { render } from "enzyme";
+import { shallow } from "enzyme";
 
 import { Header } from "components/Header";
 
 describe("Header", () => {
   it("should render correctly", () => {
-    const component = render(<Header />);
+    const component = shallow(<Header />);
 
     expect(component).toMatchSnapshot();
+  });
+
+  it("should render text 'Task Manager'", () => {
+    const component = shallow(<Header />);
+
+    expect(component.text()).toContain("Task Manager");
   });
 });
